1241453bdf29e7d70a37e6c2369b582d2f9cff3f
gxz <guoxianzhe@agora.io>
2024-10-16 20:08:09 +0800

    fix: iOS18 rgba issue

--- ios/RCTImageTools/RCTImageTools.mm
+++ ios/RCTImageTools/RCTImageTools.mm
@@ -6,38 +6,44 @@
 
 #include "RCTImageTools.h"
 #include "ImageHelpers.h"
-#import <React/RCTImageLoader.h>
+#import <React/RCTBridge.h>
+#import <React/RCTConvert.h>
+#import <React/RCTLog.h>
 
-
-@implementation ImageTools
-
-@synthesize bridge = _bridge;
+@implementation ImageTools {
+    RCTBridge *_bridge;
+}
 
 RCT_EXPORT_MODULE();
 
-bool saveImage(NSString * fullPath, UIImage * image, NSString * format, float quality)
-{
-    NSData* data = nil;
+- (instancetype)initWithBridge:(RCTBridge *)bridge {
+    if (self = [super init]) {
+        _bridge = bridge;
+    }
+    return self;
+}
+
+bool saveImage(NSString *fullPath, UIImage *image, NSString *format, float quality) {
+    NSData *data = nil;
     if ([format isEqualToString:@"JPEG"]) {
         data = UIImageJPEGRepresentation(image, quality / 100.0);
     } else if ([format isEqualToString:@"PNG"]) {
         data = UIImagePNGRepresentation(image);
     }
-    
+
     if (data == nil) {
         return NO;
     }
-    
-    NSFileManager* fileManager = [NSFileManager defaultManager];
+
+    NSFileManager *fileManager = [NSFileManager defaultManager];
     return [fileManager createFileAtPath:fullPath contents:data attributes:nil];
 }
 
-NSString * generateFilePath(NSString * ext, NSString * outputPath)
-{
-    NSString* directory;
+NSString *generateFilePath(NSString *ext, NSString *outputPath) {
+    NSString *directory;
 
     if ([outputPath length] == 0) {
-        NSArray* paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
         directory = [paths firstObject];
     } else {
         NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
@@ -51,64 +57,61 @@ NSString * generateFilePath(NSString * ext, NSString * outputPath)
         }
     }
 
-    NSString* name = [[NSUUID UUID] UUIDString];
-    NSString* fullName = [NSString stringWithFormat:@"%@.%@", name, ext];
-    NSString* fullPath = [directory stringByAppendingPathComponent:fullName];
+    NSString *name = [[NSUUID UUID] UUIDString];
+    NSString *fullName = [NSString stringWithFormat:@"%@.%@", name, ext];
+    NSString *fullPath = [directory stringByAppendingPathComponent:fullName];
 
     return fullPath;
 }
 
 typedef unsigned char byte;
 
-UIImage * bmp2Binary(UIImage * sourceImage, int threshold, UInt32 frontColor, UInt32 backColor)
-{
+UIImage *bmp2Binary(UIImage *sourceImage, int threshold, UInt32 frontColor, UInt32 backColor) {
     CGContextRef ctx;
     CGImageRef imageRef = [sourceImage CGImage];
     NSUInteger width = CGImageGetWidth(imageRef);
     NSUInteger height = CGImageGetHeight(imageRef);
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    byte *rawData = (byte*)malloc(height * width * 4);
+    byte *rawData = (byte *)malloc(height * width * 4);
     NSUInteger bytesPerPixel = 4;
     NSUInteger bytesPerRow = bytesPerPixel * width;
     NSUInteger bitsPerComponent = 8;
     CGContextRef context = CGBitmapContextCreate(rawData, width, height,
                                                  bitsPerComponent, bytesPerRow, colorSpace,
                                                  kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
-    
+
     CGContextDrawImage(context, CGRectMake(0, 0, width, height), imageRef);
     CGContextRelease(context);
 
     int byteIndex = 0;
-    for (int ii = 0 ; ii < width * height ; ++ii)
-    {
-        int grey = (rawData[byteIndex] + rawData[byteIndex+1] + rawData[byteIndex+2]);
+    for (int ii = 0; ii < width * height; ++ii) {
+        int grey = (rawData[byteIndex] + rawData[byteIndex + 1] + rawData[byteIndex + 2]);
 
-        UInt32* pixelPtr = (UInt32*)(rawData + byteIndex);
+        UInt32 *pixelPtr = (UInt32 *)(rawData + byteIndex);
 
-        if ( grey >= threshold * 3) {
+        if (grey >= threshold * 3) {
             pixelPtr[0] = backColor;
-        }
-        else {
+        } else {
             pixelPtr[0] = frontColor;
         }
-        
+
         byteIndex += 4;
     }
-    
+
     ctx = CGBitmapContextCreate(rawData,
-                                CGImageGetWidth( imageRef ),
-                                CGImageGetHeight( imageRef ),
+                                CGImageGetWidth(imageRef),
+                                CGImageGetHeight(imageRef),
                                 8,
                                 bytesPerRow,
                                 colorSpace,
-                                kCGImageAlphaPremultipliedLast ); 
+                                kCGImageAlphaPremultipliedLast);
     CGColorSpaceRelease(colorSpace);
-    
-    imageRef = CGBitmapContextCreateImage (ctx);
-    UIImage* rawImage = [UIImage imageWithCGImage:imageRef];  
+
+    imageRef = CGBitmapContextCreateImage(ctx);
+    UIImage *rawImage = [UIImage imageWithCGImage:imageRef];
     CGImageRelease(imageRef);
-    
-    CGContextRelease(ctx);  
+
+    CGContextRelease(ctx);
     free(rawData);
 
     return rawImage;
@@ -119,19 +122,17 @@ RCT_EXPORT_METHOD(createBinaryImage:(NSString *)path
                   threshold:(int)threshold
                   format:(NSString *)format
                   quality:(float)quality
-                  bOutputBase64: (BOOL)bOutputBase64
+                  bOutputBase64:(BOOL)bOutputBase64
                   frontColorString:(NSString *)frontColorString
                   backColorString:(NSString *)backColorString
-                  callback:(RCTResponseSenderBlock)callback)
-{
-    
-    //Set image extension
-    NSString *extension = @"jpg";
-    if ([format isEqualToString:@"PNG"]) {
-        extension = @"png";
-    }
+                  callback:(RCTResponseSenderBlock)callback) {
 
+    NSString *extension = [format isEqualToString:@"PNG"] ? @"png" : @"jpg";
+    NSString *fullPath;
     
+    
+    NSString* fullPath;
+
     NSString* fullPath;
     @try {
         fullPath = generateFilePath(extension, @"");
@@ -140,112 +141,116 @@ RCT_EXPORT_METHOD(createBinaryImage:(NSString *)path
         return;
     }
 
-    [_bridge.imageLoader loadImageWithURLRequest:[RCTConvert NSURLRequest:path] callback:^(NSError *error, UIImage *image) {
-        if (error || image == nil) {
-            if ([path hasPrefix:@"data:"] || [path hasPrefix:@"file:"]) {
-                NSURL *imageUrl = [[NSURL alloc] initWithString:path];
-                image = [UIImage imageWithData:[NSData dataWithContentsOfURL:imageUrl]];
-            } else {
-                image = [[UIImage alloc] initWithContentsOfFile:path];
-            }
-            if (image == nil) {
-                callback(@[@"Can't retrieve the file from the path.", @""]);
-                return;
-            }
-        }
+    NSURL *imageUrl = [NSURL URLWithString:path];
+    NSData *imageData = [NSData dataWithContentsOfURL:imageUrl];
 
-        uint frontColor = 0;
-        [[NSScanner scannerWithString:frontColorString] scanHexInt:&frontColor];
-        uint backColor = 65535;
-        [[NSScanner scannerWithString:backColorString] scanHexInt:&backColor];
-        switch (type) {
-            case 1:
-                image = bmp2Binary(image, threshold, frontColor, backColor);
-                break;
-            default:
-                break;
-        }
+    if (!imageData) {
+        callback(@[@"Can't retrieve the image from the path.", @""]);
+        return;
+    }
 
-        if (bOutputBase64) {
-            NSData * dataImage = UIImagePNGRepresentation(image);
-            NSString * base64 = [dataImage base64EncodedStringWithOptions:0];
+    UIImage *image = [UIImage imageWithData:imageData];
+    if (image == nil) {
+        callback(@[@"Can't create image from data.", @""]);
+        return;
+    }
+
+    uint frontColor = 0;
+    [[NSScanner scannerWithString:frontColorString] scanHexInt:&frontColor];
+    uint backColor = 65535;
+    [[NSScanner scannerWithString:backColorString] scanHexInt:&backColor];
+
+    if (type == 1) {
+        image = bmp2Binary(image, threshold, frontColor, backColor);
+    }
+
+    if (bOutputBase64) {
+        NSData *dataImage = UIImagePNGRepresentation(image);
+        NSString *base64 = [dataImage base64EncodedStringWithOptions:0];
 
             NSDictionary *response = @{@"base64": base64};
+            NSDictionary *response = @{@"base64": base64};
             
-            callback(@[[NSNull null], response]);
-        }
-        else {
-            // Compress and save the image
-            if (!saveImage(fullPath, image, format, quality)) {
-                callback(@[@"Can't save the image. Check your compression format and your output path", @""]);
-                return;
-            }
-            NSURL *fileUrl = [[NSURL alloc] initFileURLWithPath:fullPath];
-            NSString *fileName = fileUrl.lastPathComponent;
-            NSError *attributesError = nil;
-            NSDictionary *fileAttributes = [[NSFileManager defaultManager] attributesOfItemAtPath:fullPath error:&attributesError];
-            NSNumber *fileSize = fileAttributes == nil ? 0 : [fileAttributes objectForKey:NSFileSize];
-            NSDictionary *response = @{@"path": fullPath,
-                                    @"uri": fileUrl.absoluteString,
-                                    @"name": fileName,
-                                    @"size": fileSize == nil ? @(0) : fileSize
-                                    };
+        NSDictionary *response = @{@"base64": base64};
             
-            callback(@[[NSNull null], response]);
+        callback(@[[NSNull null], response]);
+    } else {
+        if (!saveImage(fullPath, image, format, quality)) {
+            callback(@[@"Can't save the image. Check your compression format and your output path", @""]);
+            return;
         }
-    }];
+        NSURL *fileUrl = [[NSURL alloc] initFileURLWithPath:fullPath];
+        NSString *fileName = fileUrl.lastPathComponent;
+        NSError *attributesError = nil;
+        NSDictionary *fileAttributes = [[NSFileManager defaultManager] attributesOfItemAtPath:fullPath error:&attributesError];
+        NSNumber *fileSize = fileAttributes == nil ? @(0) : [fileAttributes objectForKey:NSFileSize];
+        NSDictionary *response = @{@"path": fullPath,
+                                   @"uri": fileUrl.absoluteString,
+                                   @"name": fileName,
+                                   @"size": fileSize == nil ? @(0) : fileSize};
+
+        callback(@[[NSNull null], response]);
+    }
 }
 
 RCT_EXPORT_METHOD(GetImageRGBAs:(NSString *)path
-                  callback:(RCTResponseSenderBlock)callback)
-{
-    [_bridge.imageLoader loadImageWithURLRequest:[RCTConvert NSURLRequest:path] callback:^(NSError *error, UIImage *image) {
-        if (error || image == nil) {
-            if ([path hasPrefix:@"data:"] || [path hasPrefix:@"file:"]) {
-                NSURL *imageUrl = [[NSURL alloc] initWithString:path];
-                image = [UIImage imageWithData:[NSData dataWithContentsOfURL:imageUrl]];
-            } else {
-                image = [[UIImage alloc] initWithContentsOfFile:path];
-            }
-            if (image == nil) {
-                callback(@[@"Can't retrieve the file from the path.", @""]);
-                return;
-            }
-        }
+                  callback:(RCTResponseSenderBlock)callback) {
+    NSURL *imageUrl = [NSURL URLWithString:path];
+    NSData *imageData = nil;
 
-        CGImageRef imageRef = [image CGImage];
-        NSUInteger width = CGImageGetWidth(imageRef);
-        NSUInteger height = CGImageGetHeight(imageRef);
-        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-        byte *rawData = (byte*)malloc(height * width * 4);
-        NSUInteger bytesPerPixel = 4;
-        NSUInteger bytesPerRow = bytesPerPixel * width;
-        NSUInteger bitsPerComponent = 8;
-        CGContextRef context = CGBitmapContextCreate(rawData, width, height,
-                                                    bitsPerComponent, bytesPerRow, colorSpace,
-                                                    kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
-        
-        CGContextDrawImage(context, CGRectMake(0, 0, width, height), imageRef);
-        CGContextRelease(context);
+    if (imageUrl && imageUrl.scheme) {
+        imageData = [NSData dataWithContentsOfURL:imageUrl];
+    } else {
+        imageData = [NSData dataWithContentsOfFile:path];
+    }
 
-        NSMutableArray *pixelArray = [NSMutableArray array];
+    if (!imageData) {
+        callback(@[@"Can't retrieve the image from the path.", @""]);
+        return;
+    }
 
-        for (int ii = 0 ; ii < width * height * 4 ; ++ii)
-        {
-            [pixelArray addObject:@(rawData[ii])];
-        }
-        
-        CGColorSpaceRelease(colorSpace);
-        free(rawData);
+    UIImage *image = [UIImage imageWithData:imageData];
+    if (image == nil) {
+        callback(@[@"Can't create image from data.", @""]);
+        return;
+    }
 
-        NSNumber * imageWidth = @(image.size.width);
-        NSNumber * imageHeight = @(image.size.height);
-        NSDictionary *response = @{@"width": imageWidth,
-                                   @"height": imageHeight,
-                                   @"rgba": pixelArray
-                                   };
-        
-        callback(@[[NSNull null], response]);
-    }];
+    CGImageRef cgImage = image.CGImage;
+    NSUInteger width = CGImageGetWidth(cgImage);
+    NSUInteger height = CGImageGetHeight(cgImage);
+
+    CGDataProviderRef dataProvider = CGImageGetDataProvider(cgImage);
+    if (dataProvider == NULL) {
+        callback(@[@"Can't get data provider from image.", @""]);
+        return;
+    }
+
+    CFDataRef data = CGDataProviderCopyData(dataProvider);
+    const UInt8 *rawData = CFDataGetBytePtr(data);
+
+    NSMutableArray *rgbaArray = [NSMutableArray arrayWithCapacity:width * height * 4];
+
+    for (NSUInteger ii = 0; ii < width * height; ++ii) {
+        NSUInteger offset = ii * 4;
+        uint8_t red = rawData[offset];
+        uint8_t green = rawData[offset + 1];
+        uint8_t blue = rawData[offset + 2];
+        uint8_t alpha = rawData[offset + 3];
+
+        [rgbaArray addObject:@(red)];
+        [rgbaArray addObject:@(green)];
+        [rgbaArray addObject:@(blue)];
+        [rgbaArray addObject:@(alpha)];
+    }
+
+    CFRelease(data);
+
+    NSNumber *imageWidth = @(image.size.width);
+    NSNumber *imageHeight = @(image.size.height);
+    NSDictionary *response = @{@"width": imageWidth,
+                               @"height": imageHeight,
+                               @"rgba": rgbaArray};
+
+    callback(@[[NSNull null], response]);
 }
 @end
